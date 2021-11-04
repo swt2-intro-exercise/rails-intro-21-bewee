53a754efc8ba39c9eaa8abbfd55bd5f9fe8f2ac3
Benedikt Weber <benedikt.web23@gmail.com>
2021-11-04 18:30:59 +0100

    Tests for #4

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  it "should have first_bame, last_name and homepage" do
+    author = build :author
+    expect(author.first_name).to eq("Alan")
+    expect(author.last_name).to eq("Turing")
+    expect(author.homepage).to eq("http://wikipedia.de/Alan_Turing")
+  end
+end
