03082b4ba4618df012ca8e64d32545cfd4c983b8
Benedikt Weber <benedikt.web23@gmail.com>
2021-11-04 18:49:31 +0100

    Tests for #5

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Author, type: :model do
     expect(author.last_name).to eq("Turing")
     expect(author.homepage).to eq("http://wikipedia.de/Alan_Turing")
   end
+  
+  it "should have name" do
+    author = build :author
+    expect(author.name).to eq("Alan Turing")
+  end
 end
