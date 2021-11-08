72ffb11b6849e839371ed5283efb16c4c12e6989
Benedikt Weber <benedikt.web23@gmail.com>
2021-11-08 19:08:57 +0100

    Closes #10

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,6 +12,20 @@ class AuthorsController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+  
+  def update
+    @author = Author.find(params[:id])
+ 
+    if @author.update(author_params)
+      redirect_to @author
+    else
+      render 'edit'
+    end
+  end
   
   def show
     @author = Author.find(params[:id])
