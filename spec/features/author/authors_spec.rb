b8097a01840942813924fc36b4556f6c80835b6f
Benedikt Weber <benedikt.web23@gmail.com>
2021-11-08 19:26:10 +0100

    Tests for #11

--- spec/features/author/authors_spec.rb
+++ spec/features/author/authors_spec.rb
@@ -5,6 +5,16 @@ describe "Authors page", type: :feature do
     visit authors_path
   end
 
+  it "should list all authors" do
+    @alan = FactoryBot.create :author
+    visit authors_path
+    Author.all.each do |author| 
+      expect(page).to have_content author.name
+      expect(page).to have_link 'Show', href: author_path(author)
+      expect(page).to have_link 'Edit', href: edit_author_path(author)
+    end
+  end
+
   it "should have a link to add new authors" do
     visit authors_path
     expect(page).to have_link 'New', href: new_author_path
--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -4,6 +4,11 @@ describe "Show author page", type: :feature do
   it "should exist at 'author_path' and render withour error" do
     @alan = FactoryBot.create :author
     visit author_path(@alan)
-    expect(page).to have_text('')
+  end
+  
+  it "should have a link to edit the author", type: :featute do
+    @alan = FactoryBot.create :author
+    visit author_path(@alan)
+    expect(page).to have_link 'Edit', href: edit_author_path(@alan)
   end
 end
