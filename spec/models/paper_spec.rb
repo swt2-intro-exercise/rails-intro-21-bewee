70d6f32f43109f31415c1658af2e855003f55d03
Benedikt Weber <benedikt.web23@gmail.com>
2021-11-14 00:27:06 +0100

    Tests for #14

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should reject invalid values" do
+    paper = Paper.new(title: nil, venue: "Mind 49: 433-460", year: 1950)
+    expect(paper).to_not be_valid
+    paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: nil, year: 1950)
+    expect(paper).to_not be_valid
+    paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: nil)
+    expect(paper).to_not be_valid
+    paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: "Toastbrot")
+    expect(paper).to_not be_valid
+  end
 end
