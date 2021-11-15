87d2c4f5079830b6d97f328be9c8e563346a9e32
Benedikt Weber <benedikt.web23@gmail.com>
2021-11-15 11:17:37 +0100

    Closes #15

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ApplicationRecord
   validates :first_name, presence: true, length: { minimum: 2 }
   validates :last_name, presence: true, length: { minimum: 2 }
+  has_and_belongs_to_many :papers
 
   def name
     "#{self[:first_name]} #{self[:last_name]}"
